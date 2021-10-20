27aeb7811aba666e1c7f398016852f88720e2106
NoorMdKhan123 <noormd6400@gmail.com>
2021-10-21 04:10:47 +0530

    Welcome to employee wage computation program

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmpWagClassPrgrm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to employee wage computation program");
+        }
+    }
+}
